f9b6299af7ee7c735cf8a0508d9beda3ab22be8d
NAME <mail@gmail.com>
2023-01-19 08:09:07 -0500

    modified
    views/index.hbs

--- app.js
+++ app.js
@@ -3,37 +3,40 @@ const path = require("path");
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: './.env'})
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 });
 
-const publicDirectory = path.join(__dirname, './public');
+const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
+
+//Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false}));
+//Parse JSON bpodies (as sent by API clients)
+app.use(express.json());
 
 
 app.set("view engine", "hbs");
 
-db.connect( (error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("MySql Connected...")
-    }
-})
-
-
-app.get("/", (req, res) => {
-    // res.send("<h1>Home Pages</h1>")
-    res.render("index");
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MySql Connected...");
+  }
 });
 
+//Define Routes
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+
 app.listen(5000, () => {
-    console.log("Server started on port 5000");
-})
+  console.log("Server started on port 5000");
+});
